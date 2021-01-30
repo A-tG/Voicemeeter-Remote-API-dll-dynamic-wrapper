9db2e24a54305cbfff7df5da4cb16942c88acf40
A-tG <a3tg.developer@gmail.com>
2021-01-30 04:07:24 +0300

    Procedure VBVMR_SetParameterStringA added

--- voicemeeter remote api wrap/RemoteApiWrapper partial/SetParameters.cs
+++ voicemeeter remote api wrap/RemoteApiWrapper partial/SetParameters.cs
@@ -8,10 +8,10 @@ namespace AtgDev.Voicemeeter
         private void InitSetParameters()
         {
             m_setParameterFloat = GetReadyDelegate<VBVMR_SetParameterFloat>();
+            m_setParameterStringA = GetReadyDelegate<VBVMR_SetParameterStringA>();
         }
 
         [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Ansi)]
-        // VBVMR_SetParameterFloat(char * szParamName, float Value);
         private delegate Int32 VBVMR_SetParameterFloat([MarshalAs(UnmanagedType.LPStr)] string paramName, Single val);
         private VBVMR_SetParameterFloat m_setParameterFloat;
         /// <summary>
@@ -25,12 +25,32 @@ namespace AtgDev.Voicemeeter
         ///     -2: no server.<br/>
         ///     -3: unknown parameter<br/>
         /// </returns>
-        public Int32 SetParameterFloat(string paramName, Single val)
+        public Int32 SetParameter(string paramName, Single val)
         {
             return m_setParameterFloat(paramName, val);
         }
 
-        // VBVMR_SetParameterStringA()
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+        private delegate Int32 VBVMR_SetParameterStringA(
+            [MarshalAs(UnmanagedType.LPStr)] string paramName,
+            [MarshalAs(UnmanagedType.LPStr)] string strVal
+        );
+        private VBVMR_SetParameterStringA m_setParameterStringA;
+        /// <summary>
+        ///     Get parameter value.
+        /// </summary>
+        /// <param name="paramName">The name of the parameter (see parameters name table)</param>
+        /// <param name="strVal">The variable containing the new value.</param>
+        /// <returns>
+        ///     0: OK (no error).<br/>
+        ///     -1: error<br/>
+        ///     -2: no server.<br/>
+        ///     -3: unknown parameter<br/>
+        /// </returns>
+        public Int32 SetParameter(string paramName, string strVal)
+        {
+            return m_setParameterStringA(paramName, strVal);
+        }
 
         // VBVMR_SetParameters()
     }
