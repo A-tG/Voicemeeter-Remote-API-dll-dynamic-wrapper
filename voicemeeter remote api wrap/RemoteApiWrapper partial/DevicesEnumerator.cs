0accd584888a8c60d1c72e5c4a9653095171dea8
A-tG <a3tg.developer@gmail.com>
2021-02-03 10:56:54 +0300

    procedure VBVMR_Output_GetDeviceDescA added

--- voicemeeter remote api wrap/RemoteApiWrapper partial/DevicesEnumerator.cs
+++ voicemeeter remote api wrap/RemoteApiWrapper partial/DevicesEnumerator.cs
@@ -9,6 +9,7 @@ namespace AtgDev.Voicemeeter
         private void InitDevicesEnumerator()
         {
             m_output_getDeviceNumber = GetReadyDelegate<VBVMR_Output_GetDeviceNumber>();
+            m_output_getDeviceDescA = GetReadyDelegate<VBVMR_Output_GetDeviceDescA>();
         }
 
         private delegate Int32 VBVMR_Output_GetDeviceNumber();
@@ -24,8 +25,33 @@ namespace AtgDev.Voicemeeter
             return m_output_getDeviceNumber();
         }
 
-        // long __stdcall VBVMR_Output_GetDeviceDescA(long zindex, long * nType, char * szDeviceName, char * szHardwareId);
         
+        private delegate Int32 VBVMR_Output_GetDeviceDescA(
+            Int32 index, 
+            out Int32 type,
+            [MarshalAs(UnmanagedType.LPStr)] StringBuilder deviceName,
+            [MarshalAs(UnmanagedType.LPStr)] StringBuilder hardwareID
+        );
+        private VBVMR_Output_GetDeviceDescA m_output_getDeviceDescA;
+        /// <summary>
+        ///     Get name and hardware ID of the device according index
+        /// </summary>
+        /// <param name="index">zero based index</param>
+        /// <param name="type">Variable receiving the type</param>
+        /// <param name="deviceName">Variable receiving the the device name</param>
+        /// <param name="hardwareID">Variable receiving the the hardware ID</param>
+        /// <returns>
+        ///     0: OK (no error).<br/>
+        /// </returns>
+        public Int32 GetOutputDeviceDescriptor(Int32 index, out Int32 type, out string deviceName, out string hardwareID)
+        {
+            var devideNameSB = new StringBuilder(256);
+            var hardwareIDsb = new StringBuilder(256);
+            var resp = m_output_getDeviceDescA(index, out type, devideNameSB, hardwareIDsb);
+            deviceName = devideNameSB.ToString();
+            hardwareID = hardwareIDsb.ToString();
+            return resp;
+        }
 
         // long __stdcall VBVMR_Input_GetDeviceNumber(void);
 
