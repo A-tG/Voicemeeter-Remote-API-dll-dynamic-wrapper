fe5e9abf70aa0f3bf0964c4aa1417cc1bda06b3e
A-tG <a3tg.developer@gmail.com>
2021-02-03 21:42:22 +0300

    Update AudioCallback.cs

--- voicemeeter remote api wrap/RemoteApiWrapper partial/AudioCallback.cs
+++ voicemeeter remote api wrap/RemoteApiWrapper partial/AudioCallback.cs
@@ -9,6 +9,8 @@ namespace AtgDev.Voicemeeter
             return;
         }
 
+        // typedef long (__stdcall* T_VBVMR_VBAUDIOCALLBACK) (void* lpUser, long nCommand, void* lpData, long nnn);
+
         // long __stdcall VBVMR_AudioCallbackRegister(long mode, T_VBVMR_VBAUDIOCALLBACK pCallback, void * lpUser, char szClientName[64]);
 
         // long __stdcall VBVMR_AudioCallbackStart(void);
