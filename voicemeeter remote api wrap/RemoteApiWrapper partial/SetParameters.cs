d0038ef10c6a7a8e9acc1b035e0c3475a7fceed2
A-tG <a3tg.developer@gmail.com>
2021-02-04 01:46:10 +0300

    updated SetParameters funcion to use unicode string

--- voicemeeter remote api wrap/RemoteApiWrapper partial/SetParameters.cs
+++ voicemeeter remote api wrap/RemoteApiWrapper partial/SetParameters.cs
@@ -11,9 +11,9 @@ namespace AtgDev.Voicemeeter
             m_setParameterStringA = GetReadyDelegate<VBVMR_SetParameterStringA>();
             m_setParameterStringW = GetReadyDelegate<VBVMR_SetParameterStringW>();
             m_setParameters = GetReadyDelegate<VBVMR_SetParameters>();
+            m_setParametersW = GetReadyDelegate<VBVMR_SetParametersW>();
         }
 
-        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Ansi)]
         private delegate Int32 VBVMR_SetParameterFloat([MarshalAs(UnmanagedType.LPStr)] string paramName, Single val);
         private VBVMR_SetParameterFloat m_setParameterFloat;
         /// <summary>
@@ -76,7 +76,38 @@ namespace AtgDev.Voicemeeter
 
         private delegate Int32 VBVMR_SetParameters([MarshalAs(UnmanagedType.LPStr)] string script);
         private VBVMR_SetParameters m_setParameters;
+        /// <summary>
+        ///     Set one or several parameters by a script (&lt; 48 kB).
+        /// </summary>
+        /// <param name="script">
+        ///     Null Terminal ASCII String giving the script<br/>                  
+        ///     (script allows to change several parameters in the same time - SYNCHRO).<br/>                  
+        ///     Possible Instuction separators: ',' ';' or '\n'(CR)<br/> 
+        ///     EXAMPLE:<br/>
+        ///     <c>                
+        ///         "Strip[0].gain = -6.0<br/>                 
+        ///         Strip[0].A1 = 0<br/>                      
+        ///         Strip[0].B1 = 1<br/>                      
+        ///         Strip[1].gain = -6.0<br/>                      
+        ///         Strip[2].gain = 0.0 <br/>                     
+        ///         Strip[3].name = "Skype Caller" "
+        ///     </c>
+        /// </param>
+        /// <returns>
+        ///     0: OK (no error).<br/> 
+        ///		>0: number of line causing script error.<br/> 
+        ///		-1: error<br/> 
+        ///		-2: no server<br/> 
+        ///		-3: unexpected error<br/> 
+        ///		-4: unexpected error<br/> 
+        /// </returns>
+        public Int32 Legacy_SetParameters(string script)
+        {
+            return m_setParameters(script);
+        }
 
+        private delegate Int32 VBVMR_SetParametersW([MarshalAs(UnmanagedType.LPWStr)] string script);
+        private VBVMR_SetParametersW m_setParametersW;
         /// <summary>
         ///     Set one or several parameters by a script (&lt; 48 kB).
         /// </summary>
@@ -104,7 +135,7 @@ namespace AtgDev.Voicemeeter
         /// </returns>
         public Int32 SetParameters(string script)
         {
-            return m_setParameters(script);
+            return m_setParametersW(script);
         }
     }
 }
