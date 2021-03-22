0213723172494dd0d052413ad824998720ff1be6
A-tG <a3tg.developer@gmail.com>
2021-03-23 01:01:06 +0300

    added AudioCallback procedures

--- voicemeeter remote api wrap/RemoteApiWrapper partial/AudioCallback.cs
+++ voicemeeter remote api wrap/RemoteApiWrapper partial/AudioCallback.cs
@@ -1,22 +1,101 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace AtgDev.Voicemeeter
 {
+    using Types.AudioCallback;
+
     partial class RemoteApiWrapper
     {
         private void InitAudioCallback()
         {
-            return;
+            unsafe
+            {
+                m_audioCallbackRegister = GetReadyDelegate<VBVMR_AudioCallbackRegister>();
+            }
+            m_audioCallbackStart = GetReadyDelegate<VBVMR_AudioCallbackStart>();
+            m_audioCallbackStop = GetReadyDelegate<VBVMR_AudioCallbackStop>();
+            m_audioCallbackUnregister = GetReadyDelegate<VBVMR_AudioCallbackUnregister>();
         }
 
-        // typedef long (__stdcall* T_VBVMR_VBAUDIOCALLBACK) (void* lpUser, long nCommand, void* lpData, long nnn);
+        unsafe private delegate Int32 VBVMR_AudioCallbackRegister(
+            Mode mode,
+            Callback callback,
+            void* customDataP,
+            [MarshalAs(UnmanagedType.LPStr)] StringBuilder ClientName
+        );
+        unsafe private VBVMR_AudioCallbackRegister m_audioCallbackRegister;
+        /// <summary>
+        ///     register your audio callback function to receive real time audio buffer
+        ///     it's possible to register up to 3x different Audio Callback in the same application or in 
+		///	    3x different applications.In the same application, this is possible because Voicemeeter
+        ///     provides 3 kind of audio Streams:<br/>
+		///		    - AUDIO INPUT INSERT(to process all Voicemeeter inputs as insert)<br/>
+		///		    - AUDIO OUTPUT INSERT(to process all Voicemeeter BUS outputs as insert)<br/>
+		///		    - ALL AUDIO I/O(to process all Voicemeeter i/o).<br/>
+		///	    Note: a single callback can be used to receive the 3 possible audio streams.
+        /// </summary>
+        /// <param name="mode">callback type</param>
+        /// <param name="customDataP">Pointer that will be passed in callback first argument</param>
+        /// <param name="ClientName">Name of the application registering the Callback. / Name of the application already registered.</param>
+        /// <returns>
+        ///     0: OK (no error).<br/>
+        ///     -1: error<br/>
+        ///     1: callback already registered (by another application).<br/>
+        /// </returns>
+        unsafe public Int32 AudioCallbackRegister(Mode mode, Callback callback, void* customDataP, ref string ClientName)
+        {
+            var name = new StringBuilder(ClientName, 64);
+            var resp = m_audioCallbackRegister(mode, callback, customDataP, name);
+            ClientName = name.ToString();
+            return resp;
+        }
 
-        // long __stdcall VBVMR_AudioCallbackRegister(long mode, T_VBVMR_VBAUDIOCALLBACK pCallback, void * lpUser, char szClientName[64]);
+        private delegate Int32 VBVMR_AudioCallbackStart();
+        private VBVMR_AudioCallbackStart m_audioCallbackStart;
+        /// <summary>
+        ///     Start Audio processing
+        /// </summary>
+        /// <returns>
+        ///     0: OK (no error).<br/>
+        ///     -1: error<br/>
+        ///     -2: no callback registred.<br/>
+        /// </returns>
+        public Int32 AudioCallbackStart()
+        {
+            return m_audioCallbackStart();
+        }
 
-        // long __stdcall VBVMR_AudioCallbackStart(void);
+        private delegate Int32 VBVMR_AudioCallbackStop();
+        private VBVMR_AudioCallbackStop m_audioCallbackStop;
+        /// <summary>
+        ///     Stop Audio processing
+        /// </summary>
+        /// <returns>
+        ///     0: OK (no error).<br/>
+        ///     -1: error<br/>
+        ///     -2: no callback registred.<br/>
+        /// </returns>
+        public Int32 AudioCallbackStop()
+        {
+            return m_audioCallbackStop();
+        }
 
-        // long __stdcall VBVMR_AudioCallbackStop(void);
-
-        // long __stdcall VBVMR_AudioCallbackUnregister(void);
+        private delegate Int32 VBVMR_AudioCallbackUnregister();
+        private VBVMR_AudioCallbackUnregister m_audioCallbackUnregister;
+        /// <summary>
+        ///     unregister your callback to release voicemeeter virtual driver
+        ///     (this function will automatically call VBVMR_AudioCallbackStop() function)
+        /// </summary>
+        /// <returns>
+        ///     0: OK (no error).<br/>
+        ///     -1: error<br/>
+        ///     1: callback already unregistered.<br/>
+        /// </returns>
+        public Int32 AudioCallbackUnregister()
+        {
+            return m_audioCallbackUnregister();
+        }
     }
 }
---
+++ voicemeeter remote api wrap/Types/AudioCallback.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AtgDev.Voicemeeter.Types.AudioCallback
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct AudioInfo
+    {
+        public Int32 samplerate;
+        public Int32 samplesPerFrame;
+    }
+
+    /// <summary>
+    ///     Struct used inside Audio Callback for x86 API
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    unsafe struct AudioBuffer32
+    {
+        public Int32 samplerate;
+        public Int32 samplesPerFrame;
+        public Int32 inputsNumber;
+        public Int32 outputsNumber;
+        /// <summary>
+        ///     Array (length=inputsNumber) of pointers to frames (array with length=samplesPerFrame)<br/>
+        ///     <code>
+        ///         var frame = (Single*)audioBufferPointer->inBufferP[channelIndex];<br/>
+        ///         frame[sampleIndex] = someFloatValue;
+        ///     </code>
+        /// </summary>
+        public fixed UInt32 inBufferP[128];
+        ///<summary>
+        ///     Array (length=outputsNumber) of pointers to frames (array with length=samplesPerFrame)<br/>
+        ///     <code>
+        ///         var frame = (Single*)audioBufferPointer->outBufferP[channelIndex];<br/>
+        ///         frame[sampleIndex] = someFloatValue;
+        ///     </code>
+        /// </summary>
+        public fixed UInt32 outBufferP[128];
+    }
+
+    /// <summary>
+    ///     Struct used inside Audio Callback for x64 API 
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    unsafe struct AudioBuffer64
+    {
+        public Int32 samplerate;
+        public Int32 samplesPerFrame;
+        public Int32 inputsNumber;
+        public Int32 outputsNumber;
+        /// <summary>
+        ///     Array (length=inputsNumber) of pointers to frames (array with length=samplesPerFrame)<br/>
+        ///     <code>
+        ///         var frame = (Single*)audioBufferPointer->inBufferP[channelIndex];<br/>
+        ///         frame[sampleIndex] = someFloatValue;
+        ///     </code>
+        /// </summary>
+        public fixed UInt64 inBufferP[128];
+        ///<summary>
+        ///     Array (length=outputsNumber) of pointers to frames (array with length=samplesPerFrame)<br/>
+        ///     <code>
+        ///         var frame = (Single*)audioBufferPointer->outBufferP[channelIndex];<br/>
+        ///         frame[sampleIndex] = someFloatValue;
+        ///     </code>
+        /// </summary>
+        public fixed UInt64 outBufferP[128];
+    }
+
+    enum Command
+    {
+        Starting = 1,
+        Ending,
+        Change,
+        BufferIn = 10,
+        BufferOut,
+        BufferMain = 20
+    }
+
+    enum Mode
+    {
+        In = 1,
+        Out,
+        Main = 4
+    }
+
+    /// <summary>
+    ///     VB-AUDIO Callback is called for different task to Initialize, perform and end your process.<br/>
+    ///     VB-AUDIO Callback is part of single TIME CRITICAL Thread.<br/>
+    ///     VB-AUDIO Callback is non re-entrant (cannot be called while in process)<br/>
+    ///     VB-AUDIO Callback is supposed to be REAL TIME when called to process buffer.<br/>
+    ///     (it means that the process has to be performed as fast as possible, waiting cycles are forbidden.
+    ///     do not use O/S synchronization object, even Critical_Section can generate waiting cycle. Do not use
+    ///     system functions that can generate waiting cycle like display, disk or communication functions for example).
+    /// </summary>
+    /// <param name="customDataP">User pointer given on callback registration.</param>
+    /// <param name="command">Reason why the callback is called.</param>
+    /// <param name="callbackDataP">Pointer on structure, depending on Command/>.</param>
+    /// <param name="addData">additional data, unused</param>
+    /// <returns>
+    ///     0: always 0 (unused).<br/>
+    /// </returns>
+    unsafe delegate Int32 Callback(void* customDataP, Command command, void* callbackDataP, Int32 addData);
+}
