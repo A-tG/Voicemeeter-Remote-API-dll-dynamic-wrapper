2c94fe961b0e6f4c897a31896e99d7ff0d5d4234
A-tG <a3tg.developer@gmail.com>
2022-02-13 21:32:38 +0300

    added overloaded GetMidiMessage method

--- voicemeeter remote api wrap/RemoteApiWrapper partial/GetLevels.cs
+++ voicemeeter remote api wrap/RemoteApiWrapper partial/GetLevels.cs
@@ -65,16 +65,43 @@ namespace AtgDev.Voicemeeter
             return m_getMidiMessage(midiBuffer, bufferSize);
         }
 
-        private delegate Int32 VBVMR_SendMidiMessage(in byte[] midiBuffer, Int32 ByteMax);
+        /// <summary>
+        ///     <para>Get MIDI message from M.I.D.I. input device used by Voicemeeter M.I.D.I. mapping.</para>
+        ///     this function must be called from one thread only
+        /// </summary>
+        /// <param name="midiBuffer">
+        ///     MIDI Buffer array. Expected message size is below 4 bytes, 
+        ///     but it's recommended to use larger buffer (1024 length) to receive
+        ///     possible multiple MIDI event message in optimal way.
+        /// </param>
+        /// <returns>
+        ///     >0: number of bytes placed in buffer (2 or 3 byte for usual M.I.D.I. message)<br/>
+        ///     -1: error<br/>
+        ///     -2: no server.<br/>
+        ///     -5: no MIDI data<br/>
+        ///     -6: no MIDI data<br/>
+        /// </returns>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ArgumentException">Thrown if midiBuffer Length is 0</exception>
+        public Int32 GetMidiMessage(byte[] midiBuffer)
+        {
+            if (midiBuffer is null) throw new ArgumentNullException($"{nameof(midiBuffer)} is null");
+
+            if (midiBuffer.Length == 0) throw new ArgumentException($"{nameof(midiBuffer)} lenght is 0");
+
+            return m_getMidiMessage(midiBuffer, midiBuffer.Length);
+        }
+
+        private delegate Int32 VBVMR_SendMidiMessage([In] byte[] midiBuffer, Int32 ByteMax);
         private VBVMR_SendMidiMessage m_sendMidiMessage;
         /// <summary>
-        ///     Added in 3.02.2 / 2.0.6.2 / 1.0.8.2
+        ///     Added in 3.0.2.2 / 2.0.6.2 / 1.0.8.2
         /// </summary>
         public Int32 SendMidiMessage(byte[] midiBuffer)
         {
             if (m_sendMidiMessage is null) return ProcedureNotImportedErrorCode;
 
-            return m_sendMidiMessage(in midiBuffer, midiBuffer.Length);
+            return m_sendMidiMessage(midiBuffer, midiBuffer.Length);
         }
     }
 }
