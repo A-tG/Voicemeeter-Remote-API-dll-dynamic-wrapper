11f9a01a02fbbf522beb235a151d2003b134214f
A-tG <a3tg.developer@gmail.com>
2022-03-26 17:53:57 +0300

    added overloaded GetParameter method

--- voicemeeter remote api wrap/RemoteApiWrapper partial/GetParameters.cs
+++ voicemeeter remote api wrap/RemoteApiWrapper partial/GetParameters.cs
@@ -101,5 +101,30 @@ namespace AtgDev.Voicemeeter
             strVal = strB.ToString();
             return resp;
         }
+
+        /// <summary>
+        ///     Get parameter value. Alternative method without string allocation
+        /// </summary>
+        /// <param name="paramName">The name of the parameter (see VoicemeeterRemoteAPI parameters name table)</param>
+        /// <param name="strVal">The StringBuilder receiving the wanted value, 512 Capacity. (UTF-16)</param>
+        /// <returns>
+        ///     0: OK (no error).<br/>
+        ///     -1: error<br/>
+        ///     -2: no server.<br/>
+        ///     -3: unknown parameter<br/>
+        ///     -5: structure mismatch<br/>
+        /// </returns>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ArgumentException">Thrown if strVal Length is not 512</exception>
+        public Int32 GetParameter(string paramName, StringBuilder strVal)
+        {
+            if (strVal is null) throw new ArgumentNullException($"{nameof(strVal)} is null");
+
+            // 512 characters according to DLL documentation
+            if (strVal.Capacity != 512) throw new ArgumentException($"{nameof(strVal)} Capacity have to be 512");
+
+            var resp = m_getParameterStringW(paramName, strVal);
+            return resp;
+        }
     }
 }
