5119788ced80c5b7c88c334821ae3a15f318e340
A-tG <a3tg.developer@gmail.com>
2022-03-27 01:58:39 +0300

    optimization, added low-level methods

--- voicemeeter remote api wrap/RemoteApiWrapper partial/SetParameters.cs
+++ voicemeeter remote api wrap/RemoteApiWrapper partial/SetParameters.cs
@@ -14,7 +14,7 @@ namespace AtgDev.Voicemeeter
             GetReadyDelegate(ref m_setParametersW);
         }
 
-        private delegate Int32 VBVMR_SetParameterFloat([In, MarshalAs(UnmanagedType.LPStr)] string paramName, Single val);
+        private delegate Int32 VBVMR_SetParameterFloat(IntPtr paramNamePtr, Single val);
         private VBVMR_SetParameterFloat m_setParameterFloat;
         /// <summary>
         ///     Set a single parameter.
@@ -29,19 +29,37 @@ namespace AtgDev.Voicemeeter
         /// </returns>
         public Int32 SetParameter(string paramName, Single val)
         {
-            return m_setParameterFloat(paramName, val);
+            var paramNamePtr = Marshal.StringToHGlobalAnsi(paramName);
+            var res = m_setParameterFloat(paramNamePtr, val);
+            Marshal.FreeHGlobal(paramNamePtr);
+
+            return res;
         }
 
-        private delegate Int32 VBVMR_SetParameterStringA(
-            [In, MarshalAs(UnmanagedType.LPStr)] string paramName,
-            [In, MarshalAs(UnmanagedType.LPStr)] string strVal
-        );
+        /// <summary>
+        ///     Set a single parameter. Alternative low-level, faster method.
+        /// </summary>
+        /// <param name="paramNamePtr">Buffer pointer (null terminated ASCII) with the name of the parameter 
+        /// (see VoicemeeterRemoteAPI parameters name table)</param>
+        /// <param name="val">The variable containing the new value.</param>
+        /// <returns>
+        ///     0: OK (no error).<br/>
+        ///     -1: error<br/>
+        ///     -2: no server.<br/>
+        ///     -3: unknown parameter<br/>
+        /// </returns>
+        public Int32 SetParameter(IntPtr paramNamePtr, Single val)
+        {
+            return m_setParameterFloat(paramNamePtr, val);
+        }
+
+        private delegate Int32 VBVMR_SetParameterStringA(IntPtr paramNamePtr, IntPtr strValPtr);
         private VBVMR_SetParameterStringA m_setParameterStringA;
         /// <summary>
         ///     Get parameter value.
         /// </summary>
         /// <param name="paramName">The name of the parameter (see parameters name table)</param>
-        /// <param name="strVal">The variable containing the new value (ANSI).</param>
+        /// <param name="strVal">The variable containing the new value (ASCII).</param>
         /// <returns>
         ///     0: OK (no error).<br/>
         ///     -1: error<br/>
@@ -50,13 +68,18 @@ namespace AtgDev.Voicemeeter
         /// </returns>
         public Int32 Legacy_SetParameter(string paramName, string strVal)
         {
-            return m_setParameterStringA(paramName, strVal);
+            var paramNamePtr = Marshal.StringToHGlobalAnsi(paramName);
+            var strValPtr = Marshal.StringToHGlobalAnsi(strVal);
+
+            var resp = m_setParameterStringA(paramNamePtr, strValPtr);
+
+            Marshal.FreeHGlobal(paramNamePtr);
+            Marshal.FreeHGlobal(strValPtr);
+
+            return resp;
         }
 
-        private delegate Int32 VBVMR_SetParameterStringW(
-            [In, MarshalAs(UnmanagedType.LPStr)] string paramName,
-            [In, MarshalAs(UnmanagedType.LPWStr)] string strVal
-        );
+        private delegate Int32 VBVMR_SetParameterStringW(IntPtr paramNamePtr, IntPtr strValPtr);
         private VBVMR_SetParameterStringW m_setParameterStringW;
         /// <summary>
         ///     Get parameter value.
@@ -71,10 +94,34 @@ namespace AtgDev.Voicemeeter
         /// </returns>
         public Int32 SetParameter(string paramName, string strVal)
         {
-            return m_setParameterStringW(paramName, strVal);
+            var paramNamePtr = Marshal.StringToHGlobalAnsi(paramName);
+            var strValPtr = Marshal.StringToHGlobalUni(strVal);
+
+            var resp = m_setParameterStringW(paramNamePtr, strValPtr);
+
+            Marshal.FreeHGlobal(paramNamePtr);
+            Marshal.FreeHGlobal(strValPtr);
+
+            return resp;
         }
 
-        private delegate Int32 VBVMR_SetParameters([In, MarshalAs(UnmanagedType.LPStr)] string script);
+        /// <summary>
+        ///     Get parameter value.
+        /// </summary>
+        /// <param name="paramNamePtr">Pointer to the string (null terminated ASCII) with the name of the parameter (see parameters name table)</param>
+        /// <param name="strValPtr">Buffer pointer containing the new value. (null terminated UTF-16)</param>
+        /// <returns>
+        ///     0: OK (no error).<br/>
+        ///     -1: error<br/>
+        ///     -2: no server.<br/>
+        ///     -3: unknown parameter<br/>
+        /// </returns>
+        public Int32 SetParameter(IntPtr paramNamePtr, IntPtr strValPtr)
+        {
+            return m_setParameterStringW(paramNamePtr, strValPtr);
+        }
+
+        private delegate Int32 VBVMR_SetParameters(IntPtr scriptPtr);
         private VBVMR_SetParameters m_setParameters;
         /// <summary>
         ///     Set one or several parameters by a script (&lt; 48 kB).
@@ -103,10 +150,14 @@ namespace AtgDev.Voicemeeter
         /// </returns>
         public Int32 Legacy_SetParameters(string script)
         {
-            return m_setParameters(script);
+            var scriptPtr = Marshal.StringToHGlobalAnsi(script);
+            var res = m_setParameters(scriptPtr);
+            Marshal.FreeHGlobal(scriptPtr);
+
+            return res;
         }
 
-        private delegate Int32 VBVMR_SetParametersW([In, MarshalAs(UnmanagedType.LPWStr)] string script);
+        private delegate Int32 VBVMR_SetParametersW(IntPtr scriptPtr);
         private VBVMR_SetParametersW m_setParametersW;
         /// <summary>
         ///     Set one or several parameters by a script (&lt; 48 kB).
@@ -135,7 +186,42 @@ namespace AtgDev.Voicemeeter
         /// </returns>
         public Int32 SetParameters(string script)
         {
-            return m_setParametersW(script);
+            var scriptPtr = Marshal.StringToHGlobalUni(script);
+            var res = m_setParametersW(scriptPtr);
+            Marshal.FreeHGlobal(scriptPtr);
+
+            return res;
+        }
+
+        /// <summary>
+        ///     Set one or several parameters by a script (&lt; 48 kB). Alternative low-level, faster method.
+        /// </summary>
+        /// <param name="scriptPtr">
+        ///     Buffer pointer of the
+        ///     string giving the script (null terminated UTF-16)<br/>                  
+        ///     (script allows to change several parameters in the same time - SYNCHRO).<br/>                  
+        ///     Possible Instuction separators: ',' ';' or '\n'(CR)<br/> 
+        ///     EXAMPLE:<br/>
+        ///     <c>                
+        ///         "Strip[0].gain = -6.0<br/>                 
+        ///         Strip[0].A1 = 0<br/>                      
+        ///         Strip[0].B1 = 1<br/>                      
+        ///         Strip[1].gain = -6.0<br/>                      
+        ///         Strip[2].gain = 0.0 <br/>                     
+        ///         Strip[3].name = "Skype Caller" "
+        ///     </c>
+        /// </param>
+        /// <returns>
+        ///     0: OK (no error).<br/> 
+        ///		>0: number of line causing script error.<br/> 
+        ///		-1: error<br/> 
+        ///		-2: no server<br/> 
+        ///		-3: unexpected error<br/> 
+        ///		-4: unexpected error<br/> 
+        /// </returns>
+        public Int32 SetParameters(IntPtr scriptPtr)
+        {
+            return m_setParametersW(scriptPtr);
         }
     }
 }
