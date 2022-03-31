88a2fc783868c224cc43a1253581cdbade11697d
A-tG <a3tg.developer@gmail.com>
2022-03-31 15:30:25 +0300

    used CheckGetParameterNameLength()

--- voicemeeter remote api wrap/RemoteApiWrapper partial/GetParameters.Float.cs
+++ voicemeeter remote api wrap/RemoteApiWrapper partial/GetParameters.Float.cs
@@ -21,8 +21,7 @@ namespace AtgDev.Voicemeeter
         /// <inheritdoc cref="CheckGetParameterNameLength(string)" path="/exception"/>
         unsafe public Int32 GetParameter(string paramName, out Single val)
         {
-            var len = paramName.Length;
-            if (len > 512) throw new ArgumentException("parameter name's length must not exceed 512");
+            var len = CheckGetParameterNameLength(paramName);
 
             byte* paramNameBuff = stackalloc byte[len + 1];
             CopyStrToByteStrBuff(paramName, paramNameBuff);
